758cce028ba2f15f41ba52e22d29cd4be4b45e5b
FanyMarin <nicte-ha.marin@hotmail.com>
2020-10-01 01:13:10 -0500

    :pencil2: UPDATE: Actualizacion del modelo y rutas de 'DistribucionMacros'

--- models/DistribucionMacros.js
+++ models/DistribucionMacros.js
@@ -2,31 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const distribucionMacrosSchema = new Schema({
-  Requerimiento_calorico: {
-    Metabolismo_basal: {
-      type: Number,
-    },
-    Calorias_de_mantenimiento: {
-      type: Number,
-    },
-    Calorias_por_objetivo: {
-      type: Number,
-    },
-  },
-  Carbohidratos: {
-    type: Number,
-  },
-  Proteinas: {
-    type: Number,
-  },
-  Grasas: {
-    type: Number,
-  },
-  Macronutrientes_por_comida: {
-    Carbohidratos: Number,
-    Proteinas: Number,
-    Grasas: Number,
-  },
+  //Resultados del usuario:
+  Metabolismo_basal: Number,
+  Calorias_de_mantenimiento: Number,
+  Calorias_por_objetivo: Number,
+
+  //Macronutrientes por objetivo:
+  Carbohidratos: Number,
+  Proteinas: Number,
+  Grasas: Number,
+
+  //Macronutrientes por comida:
+  Carbohidratos_por_comida: Number,
+  Proteinas_por_comida: Number,
+  Grasas_por_comida: Number,
+
   usuario: {
     type: Schema.Types.ObjectId,
     ref: "User",
